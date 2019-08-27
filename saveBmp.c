015bae9b1a7580d32f5661953fdc0e26dd0c411d
saruman <saruman@C2PO.localdomain>
2019-08-27 20:59:16 +0200

    refactored

--- saveBmp.c
+++ saveBmp.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include "saveBmp.h"
 
-// The file header
-typedef struct {
+
+#define SIZE_BMP_HEADER 54 // size without padding
+
+// File header according to bmp file format
+struct BmpHeader {
     char padForAlingnment[2]; //pad header size to multiple of 4 bytes
     // BMP Fileheader
     char headerField[2];
@@ -24,7 +27,7 @@ typedef struct {
     uint32_t verticalResolution;
     uint32_t numColorsInPalette;
     uint32_t numImportantColors;
-} TBmpHeader;
+};
 
 // for byte access
 typedef union {
@@ -35,46 +38,24 @@ typedef union {
         uint8_t green;
         uint8_t red;
     };
-} TPixel;
+} Pixel;
 
-uint8_t* createBMP(const uint32_t* pixels, int32_t width, int32_t height, uint32_t* bmpSize)
+void writeBMP_header(struct BmpHeader* bmpHeader,
+                     int32_t width,
+                     int32_t height,
+                     uint32_t sizePixelArray)
 {
-    if (width <= 0 || pixels == NULL)
-        return NULL;
-
-    // invert y if height < 0
-    int y_direction = 1;
-    if (height < 0) {
-        height *= -1;
-        y_direction = -1;
-    }
-
-    // calculate size width padding
-    uint32_t byteWidth = (width * 3 + 3) &~ 3;       // round to multiple of 4
-    uint32_t sizePixelArray = byteWidth * height;
-    *bmpSize = 54 + sizePixelArray;
-
-    // pointer to stored bitmap
-    uint8_t* bmpBuffer = malloc(sizeof(TBmpHeader) + sizePixelArray);
-    if (bmpBuffer == NULL)
-        return NULL;
-    // pointers for easy access
-    TBmpHeader* restrict bmpHeader         = (TBmpHeader*) bmpBuffer;              // for header access in struct style
-    uint8_t* restrict bmpPixel             = bmpBuffer + sizeof(TBmpHeader);       // for target pixel access (no struct because of padding)
-    const TPixel* restrict sourcePixel     = (const TPixel* restrict) pixels;      // for source pixel access in struct style
-
-    //write header
     // BMP Fileheader
     (*bmpHeader).headerField[0]         = 'B';
     (*bmpHeader).headerField[1]         = 'M';
-    (*bmpHeader).fileSize               = *bmpSize;
-    (*bmpHeader).reserved1              = 0x6552;                           // 'Re'
-    (*bmpHeader).reserved2              = 0x656E;                           // 'ne'
-    (*bmpHeader).offsetPixelArray       = 54;
+    (*bmpHeader).fileSize               = SIZE_BMP_HEADER + sizePixelArray;
+    (*bmpHeader).reserved1              = 0x6552;         // 'Re'
+    (*bmpHeader).reserved2              = 0x656E;         // 'ne'
+    (*bmpHeader).offsetPixelArray       = SIZE_BMP_HEADER;
     //BITMAPINFOHEADER
     (*bmpHeader).headerSize             = 40;
     (*bmpHeader).bmpWidth               = width;
-    (*bmpHeader).bmpHeight              = height * y_direction;
+    (*bmpHeader).bmpHeight              = height;
     (*bmpHeader).numColorPlanes         = 1;
     (*bmpHeader).bitsPerPixel           = 24;
     (*bmpHeader).compressionMethod      = 0;
@@ -83,16 +64,52 @@ uint8_t* createBMP(const uint32_t* pixels, int32_t width, int32_t height, uint32
     (*bmpHeader).verticalResolution     = 2835;
     (*bmpHeader).numColorsInPalette     = 0;
     (*bmpHeader).numImportantColors     = 0;
+}
 
-    // write pixel array
-    int row, column;
-    for (row = 0; row < height; row++) {
-        for (column = 0; column < width; column++) {
-            bmpPixel[row * byteWidth + column * 3]     = sourcePixel[row * width + column].blue;
-            bmpPixel[row * byteWidth + column * 3 + 1] = sourcePixel[row * width + column].green;
-            bmpPixel[row * byteWidth + column * 3 + 2] = sourcePixel[row * width + column].red;
+void writeBMP_body(const Pixel* source,
+                   uint8_t* target,
+                   int32_t width,
+                   int32_t height,
+                   uint32_t paddedWidth)
+{
+    for (int32_t row = 0; row < height; row++) {
+        for (int32_t column = 0; column < width; column++) {
+            target[row * paddedWidth + column * 3]     = source[row * width + column].blue;
+            target[row * paddedWidth + column * 3 + 1] = source[row * width + column].green;
+            target[row * paddedWidth + column * 3 + 2] = source[row * width + column].red;
         }
     }
+}
+
+static inline uint32_t roundMultipleOf4(uint32_t x)
+{
+    return (x * 3 + 3) &~ 3;
+}
+
+uint8_t* createBMP(const uint32_t* pixels, int32_t width, int32_t height, uint32_t* bmpSize)
+{
+    if (width <= 0 || height == 0 || pixels == NULL)
+        return NULL;
+
+    int y_direction = 1;
+    if (height < 0) {
+        height *= -1;
+        y_direction = -1;
+    }
+
+    // calculate size width padding
+    uint32_t paddedWidth = roundMultipleOf4(width);
+    uint32_t sizePixelArray = paddedWidth * height;
+    *bmpSize = SIZE_BMP_HEADER + sizePixelArray;
+
+    uint8_t* bmpBuffer = malloc(sizeof(struct BmpHeader) + sizePixelArray);
+    if (bmpBuffer == NULL)
+        return NULL;
+
+    writeBMP_header((struct BmpHeader*)bmpBuffer, width, height * y_direction, sizePixelArray);
+
+    writeBMP_body((Pixel*)pixels, bmpBuffer + sizeof(struct BmpHeader), width, height, paddedWidth);
+
     return bmpBuffer;
 }
 
@@ -106,12 +123,12 @@ int saveBMP(const char* filename, const uint32_t* pixels, int32_t width, int32_t
     uint8_t* bmpBuffer = createBMP(pixels, width, height, &bmpSize);
     if (bmpBuffer == NULL) {
         fclose(f);
-        return -2;     // invalid parameters or error in malloc
+        return -2;
     }
     if (fwrite(bmpBuffer + 2, 1, bmpSize, f) != bmpSize) {  // BMP Data starts with offset two because of alignment
         fclose(f);
         free(bmpBuffer);
-        return -3;     // error in writing the file
+        return -3;
     }
     fclose(f);
     free(bmpBuffer);
